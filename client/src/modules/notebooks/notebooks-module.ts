e7bcfe6d9317e67e9aeb41ffa39b27ada77d0e4c
vroola05 <mark@vrooland.net>
2018-12-14 15:48:46 +0100

    Added some functions to the TabMenu.

--- client/src/modules/notebooks/notebooks-module.ts
+++ client/src/modules/notebooks/notebooks-module.ts
@@ -1,9 +1,18 @@
 import Lang from '../../components/language/lang';
 
 import {TabMenu} from '../../components/tabMenu/tab-menu';
+import { stringify } from 'querystring';
 
 export default class NotebooksModule extends TabMenu {
     constructor(){
-        super(Lang.get("notebooks_name"), "notebook");
+        let labels = new Map<string,string>([
+            ["name", Lang.get("notebooks_name")], 
+            ["add", Lang.get("notebooks_add")]
+        ]);
+        super(labels, "notebook");
+    }
+
+    public click(e:Event, identifier:number){
+        console.log("Load the chapters");
     }
 }
